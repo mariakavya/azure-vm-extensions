a8c7907f48b5dc0f44383091614c32dc974a7f5e
rogeryang777 <Roger_Yang@trend.com.tw>
2017-11-28 12:06:02 +0800

    Allow user to specify the version of DeepSecurity Extension he wants to install on ARM VM

--- DeepSecurityAddExtension_ARM_Windows_sample.ps1
+++ DeepSecurityAddExtension_ARM_Windows_sample.ps1
@@ -8,23 +8,41 @@
     # Resource group name where VM is located
     [Parameter(Mandatory=$true)][string]$resourceGroupName,
     # VM name
-    [Parameter(Mandatory=$true)][string]$vmName
+    [Parameter(Mandatory=$true)][string]$vmName,
+
+    # Publisher of Azure Extension
+    [Parameter(Mandatory=$false)][string]$extPublisher="TrendMicro.DeepSecurity",
+    # Name of Azure Extension
+    [Parameter(Mandatory=$false)][string]$extName="TrendMicroDSA",
+    # Version of Azure Extension
+    [Parameter(Mandatory=$false)][ValidateSet("9.6","10.0")][string]$extVersion
  )
-
-$dsaExtPublisher = "TrendMicro.DeepSecurity"
-$dsaExtName = "TrendMicroDSA"
-
 $protectedFileContent = Get-Content -Raw -LiteralPath $privateFileName -Encoding UTF8 -ErrorAction Stop
 $publicFileContent = Get-Content -Raw -LiteralPath $publicFileName -Encoding UTF8 -ErrorAction Stop
 
-try{
+function Get-DeepSecurityExtensionLatestVersion {
+ param (
+        [Parameter(Mandatory=$true,Position=0)][string]$location,
+        [Parameter(Mandatory=$true,Position=1)][string]$extPublisher,
+        [Parameter(Mandatory=$true,Position=2)][string]$extName
+    )
 
-    $ver = Get-AzureRmVMExtensionImage -Location $location -PublisherName $dsaExtPublisher -Type $dsaExtName -ErrorAction Stop | Sort-Object -Property Version | Select-Object -Property Version -First 1
+    $ver = Get-AzureRmVMExtensionImage -Location $location -PublisherName $extPublisher -Type $extName -ErrorAction Stop | Sort-Object -Property Version | Select-Object -Property Version -First 1
+    return $ver
+}
+
+try{
+    if($extVersion) {
+        $ver = $extVersion
+    } else {
+        $ver = Get-DeepSecurityExtensionLatestVersion $location $extPublisher $extName
+    }
 
     if( $ver -match "(\d+\.\d+)\.*") {  # get first two version-number (eg : get A.B from A.B.C.D)
         $ver = $Matches[1]
     }
-    Set-AzureRMVMExtension -ResourceGroupName $resourceGroupName -Location $location -VMName $vmName -Name $dsaExtName -Publisher $dsaExtPublisher -ExtensionType $dsaExtName -TypeHandlerVersion $ver -SettingString $publicFileContent -ProtectedSettingString $protectedFileContent -ErrorAction Stop
+
+    Set-AzureRMVMExtension -ResourceGroupName $resourceGroupName -Location $location -VMName $vmName -Name $extName -Publisher $extPublisher -ExtensionType $extName -TypeHandlerVersion $ver -SettingString $publicFileContent -ProtectedSettingString $protectedFileContent -ErrorAction Stop
     
 }catch {
    Write-Error -Exception $_.Exception
